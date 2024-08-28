5c9f58f891aeeaf9823a66275ef63d87b62ffec0
Your Name <your_email@example.com>
2024-08-28 10:59:51 -1100

    fast commit

---
+++ java.js
@@ -0,0 +1,13 @@
+const cars=[
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+  console.log("MY CARS NAME IS = ",cars,"<br><br>");
+
+  const time=11;
+  if(time<10){
+    console.log("Good morning")
+  }else{
+    console.log("Good evening")
+  }
